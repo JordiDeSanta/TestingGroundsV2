1ee9b075e9a4c043b9aa48d5bb3bf48169e788e4
Jordi <jordi.bravo.bad@gmail.com>
2020-12-20 16:50:21 -0300

    Auto stash before revert of "v4.25"

--- Source/S05_TestingGrounds/S05_TestingGrounds.Build.cs
+++ Source/S05_TestingGrounds/S05_TestingGrounds.Build.cs
@@ -6,6 +6,8 @@ public class S05_TestingGrounds : ModuleRules
 {
 	public S05_TestingGrounds(ReadOnlyTargetRules Target) : base(Target)
 	{
+		PrivatePCHHeaderFile = "S05_TestingGrounds.h";
+
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "AIModule", "GameplayTasks" });
 	}
 }
